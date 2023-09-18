b727ae16944db7b7bb6b072ca3ded5c8a77c08f7
Berat <beratergnn@gmail.com>
2023-09-18 09:03:07 +0300

    day1_2 finish

---
+++ lib/day_1/day1_2.dart
@@ -0,0 +1,29 @@
+void main() {
+  print('Hello World!');
+
+  // Değişken Tanımlama Tip Belirmeksizin
+  var yas = 34;
+
+  // Değişken Tanımlama Tip Beliterek
+  int sayi = 34;
+
+  // Primitive Tipler
+
+  // Tam Sayılar = int, Ondalıklı Sayılar = double
+  // Metinsel İfadeler = String, Mantıksal İfadeler = bool
+
+  // Değişken İsimlendirme Kuralları;
+  //  @,% ifadeler kullanılmaz. Case sensitivedir. Rakamla başlanmaz.
+
+  var studentName = "Berhat";
+  var studentAge = 23;
+  var ogrenciSize = 1.80;
+  var studentFirstLetter = "B";
+  var studentIsContinue = true;
+
+  print(studentName);
+  print(studentAge);
+  print(ogrenciSize);
+  print(studentFirstLetter);
+  print(studentIsContinue);
+}
